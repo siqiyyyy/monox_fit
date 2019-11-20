4bcd0e0a1b020910fb87c53cb17124a23cba4c27
Andreas Albert <andreas.albert@cern.ch>
2019-11-20 14:15:03 -0600

    Cleanup make_ws.py

--- makeWorkspace/README.md
+++ makeWorkspace/README.md
@@ -1,5 +1,15 @@
 This is the fitting code for the mono-x analysis. It is adapted to run on TH1D histograms.
 
-1. Run the `make_ws.py` script. This script reads the histograms from the bucoffea legacy limit scripts and converts them into a RooWorkspace. Output is saved into `mono-x.root`.
 
-2. The actual model is then created using the `runModel.py` script. This script reads the workspace from `mono-x.root`.It is designed to run all categories simulatenously, but you can suppress or enable at will by modifying: `categories = ["monojet","monov"]` in the beginning of the script. The links between the control and signal regions are set in W_constraints.py and Z_constraints.py files. This is where you would input all uncertainties. The output file, `combined_model.root` is what you will need to run limits.
+## Step 1: Converting inputs into a workspace
+This script reads the histograms from the bucoffea legacy limit scripts and converts them into a RooWorkspace. Output is saved into `mono-x.root`. Usage:
+
+```bash
+./make_ws.py /path/to/input.root --category your_category
+```
+
+use `./make_ws.py --help` for help.
+
+## Step 2: Creating the full model from the workspace
+
+The actual model is then created using the `runModel.py` script. This script reads the workspace from `mono-x.root`.It is designed to run all categories simulatenously, but you can suppress or enable at will by modifying: `categories = ["monojet","monov"]` in the beginning of the script. The links between the control and signal regions are set in W_constraints.py and Z_constraints.py files. This is where you would input all uncertainties. The output file, `combined_model.root` is what you will need to run limits.
--- makeWorkspace/make_ws.py
+++ makeWorkspace/make_ws.py
@@ -1,46 +1,75 @@
+#!/usr/bin/env python
+
 import ROOT
 ROOT.gSystem.Load("libHiggsAnalysisCombinedLimit")
 from HiggsAnalysis.CombinedLimit.ModelTools import *
+import os
+import argparse
+pjoin = os.path.join
 
-cat  = "monojet"
-fin = ROOT.TFile('/afs/cern.ch/work/a/aalbert/public/share/2019-10-16_legacylimit/legacy_limit_2017.root','READ')
-fdir = fin.Get("category_"+cat)
+def cli_args():
+    parser = argparse.ArgumentParser(prog='Convert input histograms into RooWorkspace.')
+    parser.add_argument('file', type=str, help='Input file to use.')
+    parser.add_argument('--outpath', type=str, help='Path to save output under.', default='./out')
+    parser.add_argument('--category', type=str, default=None, help='Analysis category')
+    args = parser.parse_args()
 
-fout = ROOT.TFile('mono-x.root','RECREATE')
-foutdir = fout.mkdir("category_"+cat)
+    args.file = os.path.abspath(args.file)
+    if not os.path.exists(args.file):
+      raise IOError("Input file not found: " + args.file)
+    if not args.file.endswith('.root'):
+      raise IOError("Input file is not a ROOT file: " + args.file)
+    if not args.category:
+      raise IOError("Please specify the --category option.")
+    return args
 
-wsin_combine = ROOT.RooWorkspace("wspace_"+cat,"wspace_"+cat)
-wsin_combine._import = SafeWorkspaceImporter(wsin_combine)#getattr(wsin_combine,"import")
- 
-samplehistos = fdir.GetListOfKeys()
-for s in samplehistos: 
-  obj = s.ReadObj()
-  samplehist = obj
-  break
-nbins = samplehist.GetNbinsX()
+def main():
+  # Commandline arguments
+  args = cli_args()
 
-varl = ROOT.RooRealVar("met","met",0,100000);
+  # Create output path
+  if not os.path.exists(args.outpath):
+    os.makedirs(args.outpath)
 
-# Keys in the fdir 
-keys_local = fdir.GetListOfKeys() 
-for key in keys_local: 
-  obj = key.ReadObj()
-  print obj.GetName(), obj.GetTitle(), type(obj)
-  if type(obj)!=type(ROOT.TH1D()): continue
-  title = obj.GetTitle()
-  name = obj.GetName()
-  if not obj.Integral() > 0 : obj.SetBinContent(1,0.0001) # otherwise Combine will complain!
-  print "Creating Data Hist for ", name 
-  dhist = ROOT.RooDataHist("%s"%(name),"DataSet - %s, %s"%(cat,name),ROOT.RooArgList(varl),obj)
-  wsin_combine._import(dhist)
-  
-  obj.SetDirectory(0)
+  fin = ROOT.TFile(args.file,'READ')
+  fdir = fin.Get("category_"+args.category)
+
+  fout = ROOT.TFile(pjoin(args.outpath, 'mono-x.root'),'RECREATE')
+  foutdir = fout.mkdir("category_"+args.category)
+
+  wsin_combine = ROOT.RooWorkspace("wspace_"+args.category,"wspace_"+args.category)
+  wsin_combine._import = SafeWorkspaceImporter(wsin_combine)#getattr(wsin_combine,"import")
+
+  varl = ROOT.RooRealVar("met","met",0,100000);
+
+  # Keys in the fdir
+  keys_local = fdir.GetListOfKeys()
+  for key in keys_local:
+    obj = key.ReadObj()
+    print obj.GetName(), obj.GetTitle(), type(obj)
+    if type(obj)!=type(ROOT.TH1D()):
+      continue
+    title = obj.GetTitle()
+    name = obj.GetName()
+    if not obj.Integral() > 0:
+      obj.SetBinContent(1,0.0001) # otherwise Combine will complain!
+    print "Creating Data Hist for ", name
+    dhist = ROOT.RooDataHist("%s"%(name),"DataSet - %s, %s"%(args.category,name),ROOT.RooArgList(varl),obj)
+    wsin_combine._import(dhist)
+
+
   foutdir.cd()
-  foutdir.WriteTObject(obj)
+  foutdir.WriteTObject(wsin_combine)
+  foutdir.Write()
+  fout.Write()
 
-foutdir.cd()
-foutdir.WriteTObject(wsin_combine)
-foutdir.Write()
-fout.Write()
-#wsin_combine.writeToFile("mono-x.root")
 
+  # For reasons unknown, if we do not return
+  # the workspace from this function, it goes
+  # out of scope and segfaults.
+  # ????
+  # To be resolved.
+  return wsin_combine
+
+if __name__ == "__main__":
+  a=main()
