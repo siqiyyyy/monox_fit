a4f129797e8207e74af70357acefa1ad5cc2bb25
Andreas Albert <andreas.albert@cern.ch>
2019-11-20 14:25:26 -0600

    Fixup make_ws output

--- makeWorkspace/make_ws.py
+++ makeWorkspace/make_ws.py
@@ -10,11 +10,12 @@ pjoin = os.path.join
 def cli_args():
     parser = argparse.ArgumentParser(prog='Convert input histograms into RooWorkspace.')
     parser.add_argument('file', type=str, help='Input file to use.')
-    parser.add_argument('--outpath', type=str, help='Path to save output under.', default='./out')
+    parser.add_argument('--out', type=str, help='Path to save output under.', default='mono-x.root')
     parser.add_argument('--category', type=str, default=None, help='Analysis category')
     args = parser.parse_args()
 
     args.file = os.path.abspath(args.file)
+    args.out = os.path.abspath(args.out)
     if not os.path.exists(args.file):
       raise IOError("Input file not found: " + args.file)
     if not args.file.endswith('.root'):
@@ -28,32 +29,33 @@ def main():
   args = cli_args()
 
   # Create output path
-  if not os.path.exists(args.outpath):
-    os.makedirs(args.outpath)
-
+  outdir = os.path.dirname(args.out)
+  if not os.path.exists(outdir):
+    os.makedirs(outdir)
+  
   fin = ROOT.TFile(args.file,'READ')
   fdir = fin.Get("category_"+args.category)
 
-  fout = ROOT.TFile(pjoin(args.outpath, 'mono-x.root'),'RECREATE')
+  fout = ROOT.TFile(args.out,'RECREATE')
   foutdir = fout.mkdir("category_"+args.category)
 
   wsin_combine = ROOT.RooWorkspace("wspace_"+args.category,"wspace_"+args.category)
   wsin_combine._import = SafeWorkspaceImporter(wsin_combine)#getattr(wsin_combine,"import")
-
+  
   varl = ROOT.RooRealVar("met","met",0,100000);
 
-  # Keys in the fdir
+  # Keys in the fdir 
   keys_local = fdir.GetListOfKeys()
-  for key in keys_local:
+  for key in keys_local: 
     obj = key.ReadObj()
     print obj.GetName(), obj.GetTitle(), type(obj)
-    if type(obj)!=type(ROOT.TH1D()):
+    if type(obj)!=type(ROOT.TH1D()): 
       continue
     title = obj.GetTitle()
     name = obj.GetName()
-    if not obj.Integral() > 0:
+    if not obj.Integral() > 0: 
       obj.SetBinContent(1,0.0001) # otherwise Combine will complain!
-    print "Creating Data Hist for ", name
+    print "Creating Data Hist for ", name 
     dhist = ROOT.RooDataHist("%s"%(name),"DataSet - %s, %s"%(args.category,name),ROOT.RooArgList(varl),obj)
     wsin_combine._import(dhist)
 
@@ -64,7 +66,7 @@ def main():
   fout.Write()
 
 
-  # For reasons unknown, if we do not return
+  # For reasons unknown, if we do not return 
   # the workspace from this function, it goes
   # out of scope and segfaults.
   # ????
