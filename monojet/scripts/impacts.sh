43728124ad1763745a9df5dfe54e40c330f33987
Andreas Albert <andreas.albert@cern.ch>
2020-02-21 10:01:46 -0600

    Monojet impacts

--- monojet/scripts/impacts.sh
+++ monojet/scripts/impacts.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 ### Impacts
-SIGNAL="0.5"
 mkdir -p impacts
 pushd impacts
-for YEAR in 2017 2018; do
-    mkdir ${YEAR}
-    pushd ${YEAR}
-    combineTool.py -M Impacts -d ../../cards/card_monojet_${YEAR}.root -m 125 --doInitialFit --robustFit 1 -t -1 --expectSignal=${SIGNAL} --parallel=4
-    combineTool.py -M Impacts -d ../../cards/card_monojet_${YEAR}.root -m 125 --robustFit 1 --doFits --parallel 4 -t -1 --expectSignal=${SIGNAL} --parallel=4
-    combineTool.py -M Impacts -d ../../cards/card_monojet_${YEAR}.root -m 125 -o impacts.json -t -1 --expectSignal=${SIGNAL}  --parallel=4
-    popd
-    plotImpacts.py -i ${YEAR}/impacts.json -o impacts_monojet_${YEAR}
+for SIGNAL in "0.0" "0.5"; do 
+    for YEAR in 2017 2018; do
+        mkdir ${YEAR}_${SIGNAL}
+        pushd ${YEAR}_${SIGNAL}
+        combineTool.py -M Impacts -d ../../cards/card_monojet_${YEAR}.root -m 125 --doInitialFit --robustFit 1 -t -1 --expectSignal=${SIGNAL} --parallel=4
+        combineTool.py -M Impacts -d ../../cards/card_monojet_${YEAR}.root -m 125 --robustFit 1 --doFits --parallel 4 -t -1 --expectSignal=${SIGNAL} --parallel=4
+        combineTool.py -M Impacts -d ../../cards/card_monojet_${YEAR}.root -m 125 -o impacts.json -t -1 --expectSignal=${SIGNAL}  --parallel=4
+        popd
+        plotImpacts.py -i ${YEAR}_${SIGNAL}/impacts.json -o impacts_monojet_${YEAR}_${SIGNAL}
+    done
 done
 popd
