0c4ed1046912469e9381bcd78f44cbf407990844
Andreas Albert <andreas.albert@cern.ch>
2020-02-21 12:34:02 -0600

    Cleanup: stat unc

--- makeWorkspace/W_constraints.py
+++ makeWorkspace/W_constraints.py
@@ -1,45 +1,80 @@
 import ROOT
 from counting_experiment import *
+
+# Tell RooFit to be quiet
+ROOT.RooMsgService.instance().setSilentMode(True)
+
+def do_stat_unc(histogram, proc,cid, region, CR, outfile):
+  """Add stat. unc. variations to the workspace"""
+
+  # Store formatting inputs for repeated use
+  replacement = {
+    "PROC" : proc,
+    "CONSTRAINT" : cid,
+    "REGION" : region
+  }
+
+  # Add one variation per bin
+  for b in range(1, histogram.GetNbinsX()+1):
+    err = histogram.GetBinError(b)
+    content = histogram.GetBinContent(b)
+
+    # Safety
+    if (content<=0) or (err/content < 0.001) :
+      continue
+
+    replacement["BIN"] = b
+    up = histogram.Clone("{PROC}_weights_{CONSTRAINT}_{CONSTRAINT}_stat_error_{REGION}_bin{BIN}_Up".format(**replacement))
+    up.SetBinContent(b, content + err)
+    down = histogram.Clone("{PROC}_weights_{CONSTRAINT}_{CONSTRAINT}_stat_error_{REGION}_bin{BIN}_Down".format(**replacement))
+    down.SetBinContent(b, content - err)
+    outfile.WriteTObject(up)
+    outfile.WriteTObject(down)
+
+    print "Adding an error -- ", up.GetName(),err
+    CR.add_nuisance_shape("{CONSTRAINT}_stat_error_{REGION}_bin{BIN}".format(**replacement),outfile)
+
+
 # Define how a control region(s) transfer is made by defining cmodel provide, the calling pattern must be unchanged!
-# First define simple string which will be used for the datacard 
+# First define simple string which will be used for the datacard
 model = "wjets"
 def cmodel(cid,nam,_f,_fOut, out_ws, diag):
-  
+
   # Some setup
   _fin    = _f.Get("category_%s"%cid)
   _wspace = _fin.Get("wspace_%s"%cid)
 
 
   # ############################ USER DEFINED ###########################################################
-  # First define the nominal transfer factors (histograms of signal/control, usually MC 
-  # note there are many tools available inside include/diagonalize.h for you to make 
-  # special datasets/histograms representing these and systematic effects 
-  # but for now this is just kept simple 
+  # First define the nominal transfer factors (histograms of signal/control, usually MC
+  # note there are many tools available inside include/diagonalize.h for you to make
+  # special datasets/histograms representing these and systematic effects
+  # but for now this is just kept simple
   processName  = "WJets" # Give a name of the process being modelled
-  metname      = "met"    # Observable variable name 
+  metname      = "met"    # Observable variable name
   targetmc     = _fin.Get("signal_wjets")      # define monimal (MC) of which process this config will model
   controlmc    = _fin.Get("Wmn_wjets")  # defines in / out acceptance
   controlmc_e  = _fin.Get("Wen_wjets")  # defines in / out acceptance
 
-  # Create the transfer factors and save them (not here you can also create systematic variations of these 
+  # Create the transfer factors and save them (not here you can also create systematic variations of these
   # transfer factors (named with extention _sysname_Up/Down
-  WScales = targetmc.Clone(); WScales.SetName("wmn_weights_%s"%cid)
+  WScales = targetmc.Clone("wmn_weights_%s"%cid);
   WScales.Divide(controlmc)
-  _fOut.WriteTObject(WScales)  # always write out to the directory 
+  _fOut.WriteTObject(WScales)  # always write out to the directory
 
-  WScales_e = targetmc.Clone(); WScales_e.SetName("wen_weights_%s"%cid)
+  WScales_e = targetmc.Clone("wen_weights_%s"%cid);
   WScales_e.Divide(controlmc_e)
-  _fOut.WriteTObject(WScales_e)  # always write out to the directory 
+  _fOut.WriteTObject(WScales_e)  # always write out to the directory
 
   #######################################################################################################
 
-  _bins = []  # take bins from some histogram, can choose anything but this is easy 
+  _bins = []  # take bins from some histogram, can choose anything but this is easy
   for b in range(targetmc.GetNbinsX()+1):
     _bins.append(targetmc.GetBinLowEdge(b+1))
 
-  # Here is the important bit which "Builds" the control region, make a list of control regions which 
+  # Here is the important bit which "Builds" the control region, make a list of control regions which
   # are constraining this process, each "Channel" is created with ...
-  # 	(name,_wspace,out_ws,cid+'_'+model,TRANSFERFACTORS) 
+  # 	(name,_wspace,out_ws,cid+'_'+model,TRANSFERFACTORS)
   # the second and third arguments can be left unchanged, the others instead must be set
   # TRANSFERFACTORS are what is created above, eg WScales
 
@@ -55,40 +90,9 @@ def cmodel(cid,nam,_f,_fOut, out_ws, diag):
   # note, the code will LOOK for something called NOMINAL_name_Up and NOMINAL_name_Down, where NOMINAL=WScales.GetName()
   # these must be created and writted to the same dirctory as the nominal (fDir)
 
-  #CRs[0].add_nuisance("SingleMuonEff",0.01)
-  #CRs[1].add_nuisance("SingleElEff",0.02)
 
-  # Statistical uncertainties too!, one per bin 
-  for b in range(targetmc.GetNbinsX()):
-    err = WScales.GetBinError(b+1)
-    #print "ZEYNEP MISSING:", b+1, WScales.GetBinContent(b+1), err
-    if not WScales.GetBinContent(b+1)>0: continue 
-    relerr = err/WScales.GetBinContent(b+1)
-    #print "ZEYNEP MISSING:", b+1, WScales.GetBinContent(b+1), err, relerr
-    if relerr<0.001: continue
-    byb_u = WScales.Clone(); byb_u.SetName("wmn_weights_%s_%s_stat_error_%s_bin%d_Up"%(cid,cid,"singlemuonCR",b))
-    byb_u.SetBinContent(b+1,WScales.GetBinContent(b+1)+err)
-    byb_d = WScales.Clone(); byb_d.SetName("wmn_weights_%s_%s_stat_error_%s_bin%d_Down"%(cid,cid,"singlemuonCR",b))
-    byb_d.SetBinContent(b+1,WScales.GetBinContent(b+1)-err)
-    _fOut.WriteTObject(byb_u)
-    _fOut.WriteTObject(byb_d)
-    print "Adding an error -- ", byb_u.GetName(),err
-    CRs[0].add_nuisance_shape("%s_stat_error_%s_bin%d"%(cid,"singlemuonCR",b),_fOut)
-
-  # Statistical uncertainties too!, one per bin 
-  for b in range(targetmc.GetNbinsX()):
-    err_e = WScales_e.GetBinError(b+1)
-    if not WScales_e.GetBinContent(b+1)>0: continue 
-    relerr_e = err_e/WScales_e.GetBinContent(b+1)
-    if relerr_e<0.001: continue
-    byb_u_e = WScales_e.Clone(); byb_u_e.SetName("wen_weights_%s_%s_stat_error_%s_bin%d_Up"%(cid,cid,"singleelectronCR",b))
-    byb_u_e.SetBinContent(b+1,WScales_e.GetBinContent(b+1)+err_e)
-    byb_d_e = WScales_e.Clone(); byb_d_e.SetName("wen_weights_%s_%s_stat_error_%s_bin%d_Down"%(cid,cid,"singleelectronCR",b))
-    byb_d_e.SetBinContent(b+1,WScales_e.GetBinContent(b+1)-err_e)
-    _fOut.WriteTObject(byb_u_e)
-    _fOut.WriteTObject(byb_d_e)
-    print "Adding an error -- ", byb_u_e.GetName(),err_e
-    CRs[1].add_nuisance_shape("%s_stat_error_%s_bin%d"%(cid,"singleelectronCR",b),_fOut)
+  do_stat_unc(WScales, "wmn", cid, "singlemuonCR", CRs[0],_fOut)
+  do_stat_unc(WScales_e, "wen", cid, "singleelectronCR", CRs[1],_fOut)
 
 
   if "monov" in cid:
@@ -96,7 +100,7 @@ def cmodel(cid,nam,_f,_fOut, out_ws, diag):
   else:
     tag = ""
 
-  fztoz_trig = r.TFile.Open("sys/all_trig.root") # 250 - 1400 binning 
+  fztoz_trig = r.TFile.Open("sys/all_trig.root") # 250 - 1400 binning
 
   ## Here now adding the trigger uncertainty
   wtow_trig_down = fztoz_trig.Get("trig_sys_down"+tag)
@@ -124,7 +128,7 @@ def cmodel(cid,nam,_f,_fOut, out_ws, diag):
 
   CRs[1].add_nuisance_shape("mettrig",_fOut)
 
-  fwtowpdf = r.TFile.Open("sys/wtow_pdf_sys.root") # 250 - 1400 binning                                                                                               
+  fwtowpdf = r.TFile.Open("sys/wtow_pdf_sys.root") # 250 - 1400 binning
 
   wtow_pdf_down   = fwtowpdf.Get("ratio_Down"+tag)
   ratio_wtowpdf_down = targetmc.Clone();  ratio_wtowpdf_down.SetName("wmn_weights_%s_wtowpdf_Down"%cid);
@@ -153,7 +157,7 @@ def cmodel(cid,nam,_f,_fOut, out_ws, diag):
   CRs[0].add_nuisance_shape("wtowpdf",_fOut)
   CRs[1].add_nuisance_shape("wtowpdf",_fOut)
 
-  ## Veto uncertainties  
+  ## Veto uncertainties
   fwtowveto = r.TFile.Open("sys/veto_sys.root") # 250 - 1400 binning
 
   ## Wmuon CR first
@@ -225,15 +229,15 @@ def cmodel(cid,nam,_f,_fOut, out_ws, diag):
   CRs[1].add_nuisance_shape("eveto",_fOut)
   CRs[1].add_nuisance_shape("muveto",_fOut)
   CRs[1].add_nuisance_shape("tauveto",_fOut)
-  
+
 
 
   #######################################################################################################
 
   cat = Category(model,cid,nam,_fin,_fOut,_wspace,out_ws,_bins,metname,targetmc.GetName(),CRs,diag)
   cat.setDependant("zjets","wjetssignal")  # Can use this to state that the "BASE" of this is already dependant on another process
-  # EG if the W->lv in signal is dependant on the Z->vv and then the W->mv is depenant on W->lv, then 
-  # give the arguments model,channel name from the config which defines the Z->vv => W->lv map! 
+  # EG if the W->lv in signal is dependant on the Z->vv and then the W->mv is depenant on W->lv, then
+  # give the arguments model,channel name from the config which defines the Z->vv => W->lv map!
   # Return of course
   return cat
 
