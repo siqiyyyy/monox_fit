8d01f9192b91178250e89e3b716360de67ae7108
Andreas Albert <andreas.albert@cern.ch>
2020-03-04 13:19:30 -0600

    VBF scripts

--- vbf/scripts/diagnostics.sh
+++ vbf/scripts/diagnostics.sh
@@ -10,6 +10,22 @@ for YEAR in 2017 2018; do
             --setParameters mask_vbf_signal=1 \
             -n _vbf_${YEAR} \
             ../cards/card_vbf_${YEAR}.root
-    python ${CMSSW_BASE}/src/HiggsAnalysis/CombinedLimit/test/diffNuisances.py fitDiagnostics_vbf_${YEAR}.root  -g diffnuisances_vbf_${YEAR}.root
+    python ${CMSSW_BASE}/src/HiggsAnalysis/CombinedLimit/test/diffNuisances.py \
+           fitDiagnostics_vbf_${YEAR}.root\
+           -g diffnuisances_vbf_${YEAR}.root
 done
+
+
+# Combined
+combine -M FitDiagnostics \
+        --saveShapes \
+        --saveWithUncertainties \
+        --robustFit 1 \
+        --setParameters mask_monojet_2017_signal=1,mask_monojet_2018_signal=1 \
+        -n _monojet_combined \
+        ../cards/card_vbf_combined.root
+
+python ${CMSSW_BASE}/src/HiggsAnalysis/CombinedLimit/test/diffNuisances.py \
+        fitDiagnostics_vbf_combined.root \
+        -g diffnuisances_vbf_combined.root
 popd
--- vbf/scripts/make_plots.py
+++ vbf/scripts/make_plots.py
@@ -13,24 +13,33 @@ lumi ={
 regions = ['singlemuon','dimuon','gjets','singleelectron','dielectron']
 procs = ['zmm','zee','w_weights','photon','wen','wmn']
 
-
-category='vbf'
-regions = ['singlemuon','dimuon','gjets','singleelectron','dielectron']
+### Years fit separately
 for year in [2017, 2018]:
-    outdir = './plots/{year}/'.format(year=year)
+    ws_file = './root/ws_vbf.root'.format(year=year)
     fitdiag_file = 'diagnostics/fitDiagnostics_vbf_{year}.root'.format(year=year)
-    ws_file = './root/ws_vbf_{year}.root'.format(year=year)
     diffnuis_file = 'diagnostics/diffnuisances_vbf_{year}.root'.format(year=year)
+    category='vbf_{YEAR}'.format(YEAR=year)
+    outdir = './plots/{year}/'.format(year=year)
     for region in regions:
         plotPreFitPostFit(region,     category,ws_file, fitdiag_file, outdir, lumi[year], year)
-
+    for proc in procs:
+        plot_ratio(proc, category, 'root/combined_model_vbf.root'.format(year=year), outdir, lumi[year], year)
     dataValidation("combined",  "combinedW", category, ws_file, fitdiag_file, outdir,lumi[year], year)
     dataValidation("combinedW",  "gjets", category, ws_file, fitdiag_file, outdir,lumi[year], year)
     dataValidation("combined",  "gjets", category, ws_file, fitdiag_file, outdir,lumi[year], year)
 
-    procs = ['zmm','zee','w_weights','photon','wen','wmn']
-
-    for proc in procs:
-        plot_ratio(proc, category, 'root/combined_model_vbf_{year}.root'.format(year=year), outdir, lumi[year], year)
-
     plot_nuis(diffnuis_file, outdir)
+
+
+### Years fit together
+outdir="plots/combined"
+diffnuis_file = 'diagnostics/diffnuisances_vbf_combined.root'
+plot_nuis(diffnuis_file, outdir)
+
+for year in [2017,2018]:
+    ws_file = "root/ws_vbf.root".format(year=year)
+    fitdiag_file = 'diagnostics/fitDiagnostics_vbf_combined.root'.format(year=year)
+    category='vbf_{year}'.format(year=year)
+    outdir = './plots/combined_{year}/'.format(year=year)
+    for region in regions:
+        plotPreFitPostFit(region,     category,ws_file, fitdiag_file, outdir, lumi[year], year)
