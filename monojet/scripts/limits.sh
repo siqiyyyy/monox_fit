1ef1c5856619341bda7488c8db60dd0d4d8bd176
Andreas Albert <andreas.albert@cern.ch>
2020-03-17 14:04:12 +0100

    Improve monojet limits

--- monojet/scripts/limits.sh
+++ monojet/scripts/limits.sh
@@ -3,7 +3,7 @@
 ### Asimov limit
 mkdir -p limit
 pushd limit
-for YEAR in 2017 2018; do
+for YEAR in 2017 2018 combined; do
     combine -M AsymptoticLimits -t -1 -n _monojet_${YEAR} ../cards/card_monojet_${YEAR}.root | tee log_${YEAR}.txt
 done
 popd
