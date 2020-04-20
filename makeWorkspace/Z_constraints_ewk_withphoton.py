59c178fdfb0b132af197cddd5b95685cd8f8813e
Andreas Albert <andreas.albert@cern.ch>
2020-04-20 15:59:38 +0200

    VBF: Update JES/JER

--- makeWorkspace/Z_constraints_ewk_withphoton.py
+++ makeWorkspace/Z_constraints_ewk_withphoton.py
@@ -68,8 +68,32 @@ def cmodel(cid,nam,_f,_fOut, out_ws, diag, year, convention="BU"):
 
   for c in CRs[:3]:
     c.add_nuisance('CMS_trigger{YEAR}_met'.format(YEAR=year),0.02)
-    c.add_nuisance('CMS_scale{YEAR}_j_vbf'.format(YEAR=year),0.01)
 
+
+  # JES / JER for Z/Z is 1%
+  CRs[0].add_nuisance('CMS_scale{YEAR}_j_vbf'.format(YEAR=year),0.01)
+  CRs[1].add_nuisance('CMS_scale{YEAR}_j_vbf'.format(YEAR=year),0.01)
+  CRs[0].add_nuisance('CMS_res{YEAR}_j_vbf'.format(YEAR=year),0.01)
+  CRs[1].add_nuisance('CMS_res{YEAR}_j_vbf'.format(YEAR=year),0.01)
+
+  # For Z/W, it goes the other direction
+  if year=="2017":
+    CRs[2].add_nuisance('CMS_scale{YEAR}_j_vbf'.format(YEAR=year),-0.01)
+    CRs[2].add_nuisance('CMS_res{YEAR}_j_vbf'.format(YEAR=year),-0.015)
+  elif year=="2018":
+    CRs[2].add_nuisance('CMS_scale{YEAR}_j_vbf'.format(YEAR=year),-0.01)
+    CRs[2].add_nuisance('CMS_res{YEAR}_j_vbf'.format(YEAR=year),-0.01)
+  else:
+    raise RuntimeError("Year not recognized: " + str(year))
+
+  # Z/gamma
+  CRs[3].add_nuisance('CMS_scale{YEAR}_j_vbf'.format(YEAR=year),0.03)
+  if year=="2017":
+    CRs[3].add_nuisance('CMS_res{YEAR}_j_vbf'.format(YEAR=year),0.025)
+  elif year=="2018":
+    CRs[3].add_nuisance('CMS_res{YEAR}_j_vbf'.format(YEAR=year),0.01)
+  else:
+    raise RuntimeError("Year not recognized: " + str(year))
   # ############################ USER DEFINED ###########################################################
   # Add systematics in the following, for normalisations use name, relative size (0.01 --> 1%)
   # for shapes use add_nuisance_shape with (name,_fOut)
