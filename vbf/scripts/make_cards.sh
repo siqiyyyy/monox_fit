bb7aaa189c38ff7e641ac4f78bcd1e3d04ef35c6
Andreas Albert <andreas.albert@cern.ch>
2020-06-05 18:18:30 +0200

    Update lumi unc

--- vbf/scripts/make_cards.sh
+++ vbf/scripts/make_cards.sh
@@ -9,9 +9,21 @@ for YEAR in 2017 2018; do
     sed -i "s|@YEAR|${YEAR}|g" ${CARD}
 
     if [ $YEAR -eq 2017 ]; then
-        sed -i "s|@LUMI|1.023|g" ${CARD}
+        sed -i "s|@LUMIXY|1.008|g" ${CARD}
+        sed -i "s|@LUMILS|1.003|g" ${CARD}
+        sed -i "s|@LUMIBBD|1.004|g" ${CARD}
+        sed -i "s|@LUMIDB|1.005|g" ${CARD}
+        sed -i "s|@LUMIBCC|1.003|g" ${CARD}
+        sed -i "s|@LUMIGS|1.001|g" ${CARD}
+        sed -i "s|@LUMI|1.020|g" ${CARD}
     elif [ $YEAR -eq 2018 ]; then
-        sed -i "s|@LUMI|1.025|g" ${CARD}
+        sed -i "s|@LUMIXY|1.02|g" ${CARD}
+        sed -i "s|@LUMILS|1.002|g" ${CARD}
+        sed -i "s|@LUMIBBD|1.0|g" ${CARD}
+        sed -i "s|@LUMIDB|1.0|g" ${CARD}
+        sed -i "s|@LUMIBCC|1.02|g" ${CARD}
+        sed -i "s|@LUMIGS|1.00|g" ${CARD}
+        sed -i "s|@LUMI|1.015|g" ${CARD}
     fi
     sed -i "s|combined_model.root|../root/combined_model_vbf.root|g" ${CARD}
     sed -i "s|vbf_qcd_nckw_ws_${YEAR}.root|../root/vbf_qcd_nckw_ws_${YEAR}.root|g" ${CARD}
