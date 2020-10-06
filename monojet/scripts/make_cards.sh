30445af1cc6a8900ebfcfdc4974acb8acb59738a
Andreas Albert <andreas.albert@cern.ch>
2020-10-06 09:53:22 +0200

    Monojet/V: Split lumi uncertainty

--- monojet/scripts/make_cards.sh
+++ monojet/scripts/make_cards.sh
@@ -8,12 +8,25 @@ for YEAR in 2017 2018; do
     cp ../../templates/monojet_template.txt ${CARD}
     sed -i "s|@YEAR|${YEAR}|g" ${CARD}
 
-    if [ $YEAR -eq 2017 ]; then
-        sed -i "s|@LUMI|1.025|g" ${CARD}
-    elif [ $YEAR -eq 2018 ]; then
-        sed -i "s|@LUMI|1.023|g" ${CARD}
+     if [ $YEAR -eq 2017 ]; then
+        sed -i "s|@LUMIXY|1.008|g" ${CARD}
+        sed -i "s|@LUMILS|1.003|g" ${CARD}
+        sed -i "s|@LUMIBBD|1.004|g" ${CARD}
+        sed -i "s|@LUMIDB|1.005|g" ${CARD}
+        sed -i "s|@LUMIBCC|1.003|g" ${CARD}
+        sed -i "s|@LUMIGS|1.001|g" ${CARD}
+        sed -i "s|@LUMI|1.020|g" ${CARD}
+     elif [ $YEAR -eq 2018 ]; then
+        sed -i "s|@LUMIXY|1.02|g" ${CARD}
+        sed -i "s|@LUMILS|1.002|g" ${CARD}
+        sed -i "s|@LUMIBBD|1.0|g" ${CARD}
+        sed -i "s|@LUMIDB|1.0|g" ${CARD}
+        sed -i "s|@LUMIBCC|1.02|g" ${CARD}
+        sed -i "s|@LUMIGS|1.00|g" ${CARD}
+        sed -i "s|@LUMI|1.015|g" ${CARD}
         sed -i "/prefiring/d" ${CARD}
-    fi
+     fi
+
     sed -i "s|combined_model.root|../root/combined_model_monojet.root|g" ${CARD}
     sed -i "s|monojet_qcd_ws.root|../root/monojet_qcd_ws.root|g" ${CARD}
 
