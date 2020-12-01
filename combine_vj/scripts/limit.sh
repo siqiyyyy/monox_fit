d15ab9ad3e1ddc1db31b472f63949fa7bf268b83
Andreas Albert <andreas.albert@cern.ch>
2020-12-01 18:27:35 +0100

    Update scripts

--- combine_vj/scripts/limit.sh
+++ combine_vj/scripts/limit.sh
@@ -3,6 +3,6 @@ mkdir -p limit
 pushd limit
 
 for YEAR in 2017 2018 combined; do
-    combine -M Asymptotic -t -1 -n monojet_monov_nominal_tight_${YEAR} ../cards/card_monojet_monov_nominal_tight_${YEAR}.root | tee log_${YEAR}.txt &
+    combine -M AsymptoticLimits -t -1 -n monojet_monov_nominal_${YEAR} ../cards/card_monojet_monov_nominal_${YEAR}.root | tee log_${YEAR}.txt &
 done
 popd
