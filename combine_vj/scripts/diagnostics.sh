a38d7c8c1548893b91963b7b90ddfff0cfdcc403
Andreas Albert <andreas.albert@cern.ch>
2020-12-09 19:34:36 +0100

    Minor update to diag script

--- combine_vj/scripts/diagnostics.sh
+++ combine_vj/scripts/diagnostics.sh
@@ -11,11 +11,10 @@ for YEAR in 2017 2018 combined; do
                 --setParameters 'rgx{mask_.*_signal}'=1 \
                 -n _monojet_monov_${YEAR} \
                 ../cards/card_monojet_monov_nominal_${YEAR}.root \
-                | tee diag_${YEAR}.log
-
+                | tee diag_${YEAR}.log && \
         python ${CMSSW_BASE}/src/HiggsAnalysis/CombinedLimit/test/diffNuisances.py \
                 fitDiagnostics_monojet_monov_${YEAR}.root \
                 -g diffnuisances_monojet_monov_combined_${YEAR}.root \
-                --skipFitS
+                --skipFitS | tee diffnuis_${YEAR}.log &
 done
 popd
