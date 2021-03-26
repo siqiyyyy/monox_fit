3dd30e72c819cbe86a86356782a5f62f04e13b36
Andreas Albert <andreas.albert@cern.ch>
2021-03-26 13:03:06 +0100

    Runner for limits: IO redirection

--- combine_vj/scripts/limit.sh
+++ combine_vj/scripts/limit.sh
@@ -3,6 +3,6 @@ mkdir -p limit
 pushd limit
 
 for YEAR in 2017 2018 combined; do
-    combine -M AsymptoticLimits -t -1 -n monojet_monov_nominal_${YEAR} ../cards/card_monojet_monov_nominal_${YEAR}.root --setParameters LUMISCALE=1 --freezeParameters LUMISCALE | tee log_${YEAR}.txt &
+    combine -M AsymptoticLimits -t -1 -n monojet_monov_nominal_${YEAR} ../cards/card_monojet_monov_nominal_${YEAR}.root --setParameters LUMISCALE=1 --freezeParameters LUMISCALE 2>&1 | tee log_${YEAR}.txt &
 done
 popd
